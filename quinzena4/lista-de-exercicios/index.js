34ecdc8e40a6a106c9f64ce668badec0059329ee
thelastofuslinco <35960006+thelastofuslinco@users.noreply.github.com>
2020-10-07 12:29:01 -0300

    Exercicios de interpretação de codigo

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,6 @@
+/*  Exercícios de interpretação de código
+    1. R$100 * cotação
+    2. 165 / TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+    3. Quantidade total de números 14 / 6 / 8
+    4. -10 / 1590
+*/
